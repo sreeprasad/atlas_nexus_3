2af787a9f5f16820c1e2d517357934b34fc15b37
sreeprasad govindankutty <sreeprasad.sp@gmail.com>
2026-01-10 15:12:03 -0800

    Fix turbopack config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-      };
-    }
-    return config;
-  },
   experimental: {
     serverComponentsExternalPackages: ['@noble/hashes', '@noble/curves', 'viem'],
   },
+  turbopack: {},
 };
 
 export default nextConfig;
