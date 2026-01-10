68c4ce791ffe6a7c3549b148ef5469d9912fecc7
sreeprasad govindankutty <sreeprasad.sp@gmail.com>
2026-01-10 15:34:14 -0800

    Use serverExternalPackages for Vercel

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  serverExternalPackages: [
+    '@noble/hashes',
+    '@noble/curves', 
+    '@coinbase/coinbase-sdk',
+    '@coinbase/agentkit',
+    'viem',
+  ],
+};
 
 export default nextConfig;
